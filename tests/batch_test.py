371b78793c1f5e109725999a454ee8ce0a6422a7
Yesh <yesh@aliyun.com>
2023-02-16 23:10:32 +0800

    test: add relation test

--- tests/batch_test.py
+++ tests/batch_test.py
@@ -56,6 +56,20 @@ class Server:
             + f"&password={password}"
         ), ttypes.DouyinUserLoginResponse)
 
+    def follow(self, me, user):
+        """Follow a user."""
+        return assert_ok(requests.post(
+            f"{self.base}/douyin/relation/action/?token={me.Token}"
+            + f"&to_user_id={user.UserId}&action_type=1"
+        ), ttypes.DouyinRelationActionResponse)
+
+    def unfollow(self, me, user):
+        """Unfollow a user."""
+        return assert_ok(requests.post(
+            f"{self.base}/douyin/relation/action/?token={me.Token}"
+            + f"&to_user_id={user.UserId}&action_type=2"
+        ), ttypes.DouyinRelationActionResponse)
+
 
 def random_name():
     """Generate a random name."""
@@ -65,6 +79,20 @@ def random_name():
     ))
 
 
+test_counts = {}
+def log_test(func):
+    def wrapper(*args, **kwargs):
+        name = func.__name__
+        if name not in test_counts:
+            test_counts[name] = 0
+        print(f">>> {name}({test_counts[name]}): {func.__doc__}")
+        result = func(*args, **kwargs)
+        test_counts[name] = test_counts[name] + 1
+        return result
+    return wrapper
+
+
+@log_test
 def test_user_info(s):
     """Test registering and retrieving info."""
     name = random_name()
@@ -80,7 +108,36 @@ def test_user_info(s):
     assert v.UserId == user.UserId
 
 
+@log_test
+def test_relation(s):
+    """Test following, unfollowing and counters."""
+    users = []
+    password = "password"
+    for i in range(10):
+        name = random_name()
+        users.append(s.register_user(name, password))
+    for i, me in enumerate(users):
+        for j, user in enumerate(users[i+1:]):
+            s.follow(user, me)
+            u = cast_ttype(
+                s.user_info(me).User,
+                ttypes.User
+            )
+            assert u.FollowerCount == j + 1
+            assert u.FollowCount == i
+    for i, me in enumerate(users):
+        for j, user in enumerate(users[i+1:]):
+            s.unfollow(user, me)
+            u = cast_ttype(
+                s.user_info(me).User,
+                ttypes.User
+            )
+            assert u.FollowerCount == 10 - i - j - 2
+            assert u.FollowCount == 0
+
+
 if __name__ == "__main__":
     s = Server("http://127.0.0.1:8000")
     for i in range(10):
         test_user_info(s)
+    test_relation(s)
