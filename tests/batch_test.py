5962dc88f1af27a9e72a4f5f01e9ae4e269a5b05
Yesh <yesh@aliyun.com>
2023-02-17 12:04:51 +0800

    test: add follow list tests

--- tests/batch_test.py
+++ tests/batch_test.py
@@ -20,7 +20,12 @@ def cast_ttype(json, ttype):
     )
 
 
-def assert_ok(response, ttype):
+def cast_ttype_array(array, ttype):
+    """Convert an array of the type."""
+    return list(map(lambda j: cast_ttype(j, ttype), array))
+
+
+def assert_ok(response: requests.Response, ttype):
     """Assert and return the inner json."""
     assert response.status_code == 200
     json = response.json()
@@ -31,44 +36,51 @@ def assert_ok(response, ttype):
 class Server:
     """The mdouyin server instance."""
 
-    def __init__(self, base):
+    def __init__(self, base: str):
         """Initialize the server address."""
         self.base = base
 
-    def register_user(self, name, password):
+    def register_user(self, name: str, password: str) -> ttypes.DouyinUserRegisterResponse:
         """Register a new user, returning the info."""
         return assert_ok(requests.post(
-            f"{self.base}/douyin/user/register/?username={name}"
-            + f"&password={password}"
+            f"{self.base}/douyin/user/register/?username={name}&password={password}"
         ), ttypes.DouyinUserRegisterResponse)
 
-    def user_info(self, user):
+    def user_info(self, user: ttypes.DouyinUserLoginResponse) -> ttypes.DouyinUserResponse:
         """Retrieve user info."""
         return assert_ok(requests.get(
-            f"{self.base}/douyin/user/?user_id={user.UserId}"
-            + f"&token={user.Token}"
+            f"{self.base}/douyin/user/?user_id={user.UserId}&token={user.Token}"
         ), ttypes.DouyinUserResponse)
 
-    def login(self, name, password):
+    def login(self, name: str, password: str) -> ttypes.DouyinUserLoginResponse:
         """Login."""
         return assert_ok(requests.post(
-            f"{self.base}/douyin/user/login/?username={name}"
-            + f"&password={password}"
+            f"{self.base}/douyin/user/login/?username={name}&password={password}"
         ), ttypes.DouyinUserLoginResponse)
 
-    def follow(self, me, user):
+    def follow(self, me: ttypes.DouyinUserLoginResponse, user: ttypes.DouyinUserLoginResponse):
         """Follow a user."""
         return assert_ok(requests.post(
-            f"{self.base}/douyin/relation/action/?token={me.Token}"
-            + f"&to_user_id={user.UserId}&action_type=1"
+            f"{self.base}/douyin/relation/action/?token={me.Token}&to_user_id={user.UserId}&action_type=1"
         ), ttypes.DouyinRelationActionResponse)
 
-    def unfollow(self, me, user):
+    def unfollow(self, me: ttypes.DouyinUserLoginResponse, user: ttypes.DouyinUserLoginResponse):
         """Unfollow a user."""
         return assert_ok(requests.post(
-            f"{self.base}/douyin/relation/action/?token={me.Token}"
-            + f"&to_user_id={user.UserId}&action_type=2"
+            f"{self.base}/douyin/relation/action/?token={me.Token}&to_user_id={user.UserId}&action_type=2"
         ), ttypes.DouyinRelationActionResponse)
+
+    def list_follow(self, user: ttypes.DouyinUserLoginResponse) -> ttypes.DouyinRelationFollowListResponse:
+        """List following users."""
+        return assert_ok(requests.get(
+            f"{self.base}/douyin/relation/follow/list/?token={user.Token}&user_id={user.UserId}"
+        ), ttypes.DouyinRelationFollowListResponse)
+
+    def list_follower(self, user: ttypes.DouyinUserLoginResponse) -> ttypes.DouyinRelationFollowerListResponse:
+        """List following users."""
+        return assert_ok(requests.get(
+            f"{self.base}/douyin/relation/follower/list/?token={user.Token}&user_id={user.UserId}"
+        ), ttypes.DouyinRelationFollowerListResponse)
 
 
 def random_name():
@@ -79,21 +91,20 @@ def random_name():
     ))
 
 
-test_counts = {}
 def log_test(func):
+    count = 0
     def wrapper(*args, **kwargs):
+        nonlocal count
         name = func.__name__
-        if name not in test_counts:
-            test_counts[name] = 0
-        print(f">>> {name}({test_counts[name]}): {func.__doc__}")
+        print(f">>> {name}({count}): {func.__doc__}")
         result = func(*args, **kwargs)
-        test_counts[name] = test_counts[name] + 1
+        count = count + 1
         return result
     return wrapper
 
 
 @log_test
-def test_user_info(s):
+def test_user_info(s: Server):
     """Test registering and retrieving info."""
     name = random_name()
     password = "password"
@@ -109,7 +120,7 @@ def test_user_info(s):
 
 
 @log_test
-def test_relation(s):
+def test_relation(s: Server, test=None):
     """Test following, unfollowing and counters."""
     users = []
     password = "password"
@@ -125,6 +136,8 @@ def test_relation(s):
             )
             assert u.FollowerCount == j + 1
             assert u.FollowCount == i
+    if test != None:
+        test(users)
     for i, me in enumerate(users):
         for j, user in enumerate(users[i+1:]):
             s.unfollow(user, me)
@@ -136,8 +149,30 @@ def test_relation(s):
             assert u.FollowCount == 0
 
 
+@log_test
+def test_follow_list(s: Server):
+    """Test follow, follower listing."""
+    def assert_list_match(users, list):
+        assert len(users) == len(list)
+        ids = []
+        for user in users:
+            ids.append(user.Id)
+        for user in list:
+            assert user.UserId in ids
+    def tester(users):
+        for i, user in enumerate(users):
+            list = s.list_follower(user)
+            assert_list_match(cast_ttype_array(list.UserList, ttypes.User), users[i+1:])
+            list = s.list_follow(user)
+            assert_list_match(cast_ttype_array(list.UserList, ttypes.User), users[:i])
+    test_relation(s, test=tester)
+
+
 if __name__ == "__main__":
     s = Server("http://127.0.0.1:8000")
     for i in range(10):
         test_user_info(s)
-    test_relation(s)
+    for i in range(3):
+        test_relation(s)
+    for i in range(3):
+        test_follow_list(s)
